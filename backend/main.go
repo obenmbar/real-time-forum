1ce0be80d17199cfca368821d8d19a107f6181df
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-02-14 04:25:44 +0100

    update

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+
 	"fmt"
+	forum "forum/utils"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db, err := forum.InitialeDb()
+  if err != nil {
+	fmt.Println(err)
+	return 
+  }
+  defer db.Close()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
 	})
 	fmt.Println("server started: http://localhost:8080")
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Println("error in starting server")
 		return
 	}
+
 }
