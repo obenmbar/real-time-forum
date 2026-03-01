c2deeb5a54a50fa935365a3a32cea54c8c742f7b
obenmbr <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-03-01 01:40:56 +0000

    struct..go

---
+++ backend/utils/struct.go
@@ -0,0 +1,15 @@
+package forum
+
+
+
+type Users struct {
+	Nickname         string `json:"nickname"`
+	FirstName        string `json:"first_name"`
+	LastName         string `json:"last_name"`
+	Age              string `json:"age"`
+	Gender           string `json:"gender"`
+	Email            string `json:"email"`
+	Password         string `json:"password"`
+	Confirm_password string `json:"confirm-password"`
+}
+
