d130fb6a21a975525cbf2eaaa1d75305db318879
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-04-02 23:12:07 +0100

    update

--- backend/handlers/stile.go
+++ backend/handlers/stile.go
@@ -9,7 +9,6 @@ func SafeFileServer() http.Handler {
 	handler := http.FileServer(http.Dir("../frontend"))
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		if strings.HasSuffix(r.URL.Path, "/") {
 			http.Error(w, "Access Forbidden", http.StatusForbidden)
 			return
@@ -17,6 +16,9 @@ func SafeFileServer() http.Handler {
 		if strings.Contains(r.URL.Path, "/.") {
 			http.Error(w, "Access Forbidden", http.StatusForbidden)
 			return
+		}
+		if strings.HasSuffix(r.URL.Path, ".js") {
+			w.Header().Set("Content-Type", "application/javascript")
 		}
 		handler.ServeHTTP(w, r)
 	})
