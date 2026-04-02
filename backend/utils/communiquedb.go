28607283b4aef7a8ae35724dddec84088f22129c
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-04-02 23:12:36 +0100

    update

--- backend/utils/communiquedb.go
+++ backend/utils/communiquedb.go
@@ -7,7 +7,7 @@ import (
 )
 
 func InsertUser(db *sql.DB, data Users, Id string, hashpassword string) error {
-	query := "INSERT INTO users (id,nickname,age,gender,first_name_last_name,email,password)VALUES(?,?,?,?,?,?,?)"
+	query := "INSERT INTO users (id,nickname,age,gender,first_name,last_name,email,password)VALUES(?,?,?,?,?,?,?,?)"
 
 	_, err := db.Exec(query, Id, data.Nickname, data.Age, data.Gender, data.FirstName, data.LastName, data.Email, data.Password)
 	if err != nil {
